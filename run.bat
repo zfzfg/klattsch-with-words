87bce8c56cdb8708f3b38151bb71a0b1ac4293fc
zfzfg <163775060+zfzfg@users.noreply.github.com>
2026-06-29 13:59:02 +0200

    Delete smth

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starte lokalen Server fuer Klattsch...
-echo.
-echo Oeffne deinen Browser und gehe zu: http://localhost:8088/
-echo (Druecke STRG+C, um den Server zu beenden)
-echo.
-python -m http.server 8088
-pause
